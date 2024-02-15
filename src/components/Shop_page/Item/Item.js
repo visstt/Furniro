9c7c918bc787fe361bac7ad74c174bcd64040fb2
visstt <egorskomorohov020606@gmail.com>
2024-02-15 21:35:50 +0500

    add cart

--- src/components/Shop_page/Item/Item.js
+++ src/components/Shop_page/Item/Item.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 import './Item.css'
-import { Link } from 'react-router-dom'
 export class Item extends Component {
   render() {
     return (
       <div className="item">
         <div className="cart_add_container">
           <img src={"./img/" + this.props.item.img} alt="" />
-        <Link to={'/'}> <button className='btn_cart' >Add to cart</button> </Link>
         </div>
         <div className="text_item">
             <h2>{this.props.item.title}</h2>
             <p>{this.props.item.desc}</p>
-            <b>{this.props.item.price}</b>
-            <div className="plus" onClick={() => this.props.onAdd(this.props.item)}>+</div>
+            <b>{this.props.item.price}<div className="plus">+</div></b>
+            
         </div>
         
       </div>
